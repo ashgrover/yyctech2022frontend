b59a4440b25199dc60a57a314ff427b81ba60be0
bbek101-svg <84989535+bbek101-svg@users.noreply.github.com>
2022-07-23 15:48:23 -0600

    Sidebar updated

--- pages/index.js
+++ pages/index.js
@@ -10,13 +10,16 @@ export default function Home() {
     <Layout>
     <div className="App">
       <Grid container spacing={3}>
+        <Grid item xs={12} md={2} lg={2}>
+        <EventCaterogies />
+          </Grid>
         <Grid item xs={6} md={6} lg={6}>
           <EventCard />
         </Grid>
-        <Grid item xs={6} md={6} lg={6}>
+        <Grid item xs={6} md={4} lg={4}>
           <MapsAPI />
         </Grid>
-        <EventCaterogies />
+        
 
       </Grid>
     </div>
