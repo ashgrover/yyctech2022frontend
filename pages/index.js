e80a70456c6b585ddd55ab028731dd5e28acfbac
bbek101-svg <84989535+bbek101-svg@users.noreply.github.com>
2022-07-23 15:43:00 -0600

    Category Sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,25 @@
 import { Grid } from "@mui/material";
 import EventCard from "../components/EventCard";
+import EventCaterogies from "../components/EventCaterogies";
 import Layout from "../components/Layout";
 import MapsAPI from "../components/MapsAPI";
 import Sidebar from "../components/Sidebar";
 
-
 export default function Home() {
   return (
     <Layout>
-      <div className="App">
-        <Grid container spacing={3}>
-          <Grid item xs={6} md={6} lg={6}>
-            <EventCard />
-          </Grid>
-          <Grid item xs={6} md={6} lg={6}>
-            <MapsAPI />
-          </Grid>
-          <Sidebar />
-
+    <div className="App">
+      <Grid container spacing={3}>
+        <Grid item xs={6} md={6} lg={6}>
+          <EventCard />
         </Grid>
-      </div>
-    </Layout>
+        <Grid item xs={6} md={6} lg={6}>
+          <MapsAPI />
+        </Grid>
+        <EventCaterogies />
+
+      </Grid>
+    </div>
+  </Layout>
   )
 }
