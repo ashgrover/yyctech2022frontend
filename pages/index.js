cce48f18853667de8e5f3c342728c009dcd34213
Ash <ashgrover@gmail.com>
2022-07-23 17:33:05 -0600

    Changes

--- pages/index.js
+++ pages/index.js
@@ -6,23 +6,21 @@ import MapsAPI from "../components/MapsAPI";
 import Sidebar from "../components/Sidebar";
 
 export default function Home() {
-  return (
-    <Layout>
-    <div className="App">
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={2} lg={2}>
-        <EventCaterogies />
-          </Grid>
-        <Grid item xs={6} md={6} lg={6}>
-          <EventCard />
-        </Grid>
-        <Grid item xs={6} md={4} lg={4}>
-          <MapsAPI />
-        </Grid>
-        
-
-      </Grid>
-    </div>
-  </Layout>
-  )
+    return (
+        <Layout>
+            <div className="App">
+                <Grid container spacing={3}>
+                    <Grid item xs={12} md={2} lg={2}>
+                        <EventCaterogies />
+                    </Grid>
+                    <Grid item xs={6} md={6} lg={6}>
+                        <EventCard />
+                    </Grid>
+                    <Grid item xs={6} md={4} lg={4}>
+                        {/* <MapsAPI /> */}
+                    </Grid>
+                </Grid>
+            </div>
+        </Layout>
+    )
 }
