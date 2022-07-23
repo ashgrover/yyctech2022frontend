c7bbe3a0cbc371ba2360a892afee6627b17ae644
Ash <ashgrover@gmail.com>
2022-07-23 15:31:28 -0600

    Add navbar

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
+import Navbar from "./Navbar";
 
 
 export default function Layout({ children }) {
 
     return (
         <div>
-        { children }
+            <Navbar />
+            {children}
         </div>
     );
 }
--- components/MapsAPI.jsx
+++ components/MapsAPI.jsx
@@ -1,24 +1,23 @@
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 const mapStyles = {
-   maxWidth: '50vw',
-   height: '100%'
- };
+  maxWidth: '50vw',
+  height: '100%'
+};
 
-function MapsAPI({google}) {
+function MapsAPI({ google }) {
   return (
-   <Map
-   google={google}
-   zoom={8}
-   style={mapStyles}
-   initialCenter={{ lat: 47.444, lng: -122.176}}
- >
-   <Marker position={{ lat: 48.00, lng: -122.00}} />
-        </Map>
+    <Map
+      google={google}
+      zoom={8}
+      style={mapStyles}
+      initialCenter={{ lat: 47.444, lng: -122.176 }}
+    >
+      <Marker position={{ lat: 48.00, lng: -122.00 }} />
+    </Map>
   );
 }
 
 export default GoogleApiWrapper({
-   apiKey: 'AIzaSyBFeyT9tNIbTLO4qTuIlarn-PS2dyApBZM'
- })(MapsAPI);
- 
+  apiKey: 'AIzaSyBFeyT9tNIbTLO4qTuIlarn-PS2dyApBZM'
+})(MapsAPI);
